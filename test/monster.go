07fd3d5b69269260e11cf9c01adc73ae6f87f1fd
mslx <1623917571@qq.com>
2022-07-31 17:03:50 +0800

    测试github添加自定义包

---
+++ test/monster.go
@@ -0,0 +1,47 @@
+package unit
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Monster struct {
+	Name  string
+	Age   int
+	Skill string
+}
+
+// Monster 绑定方法Store 将一个Monstar变量序列化到文件中
+func (this *Monster) Store() bool {
+	data, err := json.Marshal(this)
+	if err != nil {
+		fmt.Println("marshal err=", err)
+		return false
+	}
+	// 保存到文件
+	filepath := "./json.txt"
+	err = ioutil.WriteFile(filepath, data, 0666)
+	if err != nil {
+		fmt.Println("write file err = ", err)
+		return false
+	}
+	return true
+}
+
+// 给Monster绑定方法ReStore 将一个序列化的Monster，从文件中读取
+// 反序列化为Monster对象，检查反序列化 名字正确
+func (this *Monster) ReStore() bool {
+	filepath := "./json.txt"
+	data, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		fmt.Println("ReadFile err=", err)
+		return false
+	}
+	err = json.Unmarshal(data, this)
+	if err != nil {
+		fmt.Println("UnMarshal err=", err)
+		return false
+	}
+	return true
+}
